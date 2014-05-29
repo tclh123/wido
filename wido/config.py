811062e46bc4b7ed5a61decb343f55cd3801209b
tclh123 <tclh123@gmail.com>
2014-05-29 16:43:40 +0800

    fix get_access_token

--- wido/config.py
+++ wido/config.py
@@ -2,6 +2,12 @@
 
 # add config here
 
+# from weiciyuan, https://github.com/qii/weiciyuan/blob/479168a3d2458cbc8a9dcdfb2be5616fc1ccdd7a/src/org/qii/weiciyuan/dao/URLHelper.java#L14
+APP_KEY = '1065511513'                                          # app key
+APP_SECRET = 'df428e88aae8bd31f20481d149c856ed'                 # app secret
+CALLBACK_URL = 'https://api.weibo.com/oauth2/default.html'      # callback url
+
+
 try:
     from local_config import *
 except:
--- wido/get_access_token.py
+++ wido/get_access_token.py
@@ -1,34 +1,36 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from snspy import APIClient
-from snspy import SinaWeiboMixin
+import webbrowser
+
+from lib.snspy import APIClient
+from lib.snspy import SinaWeiboMixin
 
 from config import APP_KEY, APP_SECRET, CALLBACK_URL
 
-client = APIClient(SinaWeiboMixin, app_key=APP_KEY, app_secret=APP_SECRET,
-                   redirect_uri=CALLBACK_URL)
 
-authorize_url = client.get_authorize_url()
-print 'please go %s with your browser. LOL' % authorize_url
+if __name__ == '__main__':
+    client = APIClient(SinaWeiboMixin, app_key=APP_KEY, app_secret=APP_SECRET,
+                       redirect_uri=CALLBACK_URL)
 
-code = raw_input("input CODE:")  # 166e02a2502c6804ae6b54912dde1bc3
+    authorize_url = client.get_authorize_url()
+    print 'please go %s with your browser. LOL' % authorize_url
+    webbrowser.open(authorize_url)
 
-r = client.request_access_token(code)
-access_token = r.access_token  # access token，e.g., abc123xyz456
-expires = r.expires      # token expires time, UNIX timestamp, e.g., 1384826449.252 (10:01 am, 19 Nov 2013, UTC+8:00)
+    code = raw_input("input CODE:")  # 166e02a2502c6804ae6b54912dde1bc3
 
-print "access_token: %s\nexpires: %s" % (access_token, expires)
+    r = client.request_access_token(code)
+    access_token = r.access_token  # access token，e.g., abc123xyz456
+    expires = r.expires      # token expires time, UNIX timestamp, e.g., 1384826449.252 (10:01 am, 19 Nov 2013, UTC+8:00)
 
-# FIXME: can't store???
-# TODO: auto get_access_token...!!!!
-with open('access_token.bak', 'w') as f:
-    f.write("%s\n" % access_token)
-    f.write("%s\n" % expires)
+    print "access_token: %s\nexpires: %s" % (access_token, expires)
 
-# access_token: 2.007xjRoB0s5vYMc99d8b88fePfaoaD
-# expires: 1558585961
+    with open('access_token.bak', 'w') as f:
+        f.write("%s\n" % access_token)
+        f.write("%s\n" % expires)
 
+    # access_token: 2.007xjRoB0s5vYMc99d8b88fePfaoaD
+    # expires: 1558585961
 
-# 2.007xjRoB4FD2YC43bbb2dda6z7W7KE
-# 1403550002
+    # 2.007xjRoB4FD2YC43bbb2dda6z7W7KE
+    # 1403550002
